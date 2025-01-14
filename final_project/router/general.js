73e8e19108818b04b1b063ddeb0b88be8f50dd98
kimalysha93 <kimalysha93@gmail.com>
2025-01-14 16:22:45 -0500

    register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,38 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+    return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// register a new user
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // check if both username and password are provided
+  if (username && password) {
+    // check if the user does not already exist
+    if (!doesExist(username)) {
+      // Add new user to the users array
+      users.push({ username: username, password: password });
+      return res.status(200).json({ message: "User successfully registered." });
+    } else {
+      return res.status(404).json({ messgae: "User a;ready exists" });
+    }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
