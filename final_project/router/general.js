56fd88e5f0aa42b04db068a60d77e5fd2adae278
kimalysha93 <kimalysha93@gmail.com>
2025-01-14 14:25:38 -0500

    search by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,9 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  // Retrieve the author parameter from the request URL and send corresponding book details
+  const isbn = req.params.author;
+  res.send(books[author]);
 });
 
 // Get all books based on title
