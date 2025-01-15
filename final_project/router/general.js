26730451d021d7db02ad7ccfe07ee8f3ccef3168
kimalysha93 <kimalysha93@gmail.com>
2025-01-15 15:11:55 -0500

    async get isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,11 +50,17 @@ public_users.get("/", function (req, res) {
   });
 });
 
+async function getBooksByISBN(isbn) {
+  return books[isbn];
+}
+
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
   // Retrieve the isbn parameter from the request URL and send corresponding book details
   const isbn = req.params.isbn;
-  res.send(books[isbn]);
+  getBooksByISBN(isbn).then(function (value) {
+    res.send(value);
+  });
 });
 
 // Get book details based on author
